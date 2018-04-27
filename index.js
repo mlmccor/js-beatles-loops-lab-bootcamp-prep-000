d3af35d705afda56dcd42808c36f2af8000108b6
Matthew McCormack <mmccormack1182@gmail.com>
2018-04-27 13:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   var i=0
+<<<<<<< HEAD
   while(i < facts.length) {
+=======
+  while(i <= facts.length) {
+>>>>>>> 0b1ad685c96cc0cffa57136aa334c3c30aedae4b
  facts[i]=facts[i]+"!!!";
   i++
 }  return facts
@@ -21,4 +25,8 @@ function iLoveTheBeatles(a) {
   a++
   } while (a<15)
   return b
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 0b1ad685c96cc0cffa57136aa334c3c30aedae4b
